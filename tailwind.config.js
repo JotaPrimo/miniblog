c70371e573e808001b5b73afc83ce8519f89e695
JotaPrimo <gestald118@gmail.com>
2023-06-29 19:11:37 -0400

    projeto criado

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      minHeight: {
+        '60vh': '60vh',
+      },
+    },
+  },
+  plugins: [],
+}
