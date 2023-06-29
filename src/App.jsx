bdc6c61229389e557d1b3f9be0dcb4e9c515a6a7
JotaPrimo <gestald118@gmail.com>
2023-06-29 19:36:44 -0400

    123. Adicionando componente de navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,30 @@ import "./App.css";
 //imports do route
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-
 // pages
 import About from "./pages/About/About";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
 
-
+// components
+import Navbar from "./componets/Nabvar/Navbar";
+import Footer from "./componets/Footer/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <div className="container">
           <Routes>
-            <Route path="/" element={ <Home />}></Route>
-            <Route path="/about" element={ <About /> }></Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
 
             {/** NOT FOUND */}
-            <Route path="*" element={<NotFound /> }></Route>
+            <Route path="*" element={<NotFound />}></Route>
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </>
   );
---
+++ src/componets/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer>
+        <h3>Em que você está pensando</h3>
+        <p>Mini blog &copy; 2023</p>
+    </footer>
+  )
+}
+
+export default Footer
