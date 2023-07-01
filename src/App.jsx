ab0eb00bc84f40b730ff34d3a750b9c4a291f144
JotaPrimo <gestald118@gmail.com>
2023-07-01 17:45:42 -0400

    131. Contexto da autenticação

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 //imports do route
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+import { AuthProvider } from "./context/AuthContext";
+
 // pages
 import About from "./pages/About/About";
 import Home from "./pages/Home/Home";
@@ -17,24 +19,26 @@ import Register from "./pages/Register/Register";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-          <br />
-          <br />
-          <br />
-          <Routes>
-            <Route path="/" element={ <Home /> }></Route>
-            <Route path="/about" element={ <About /> }></Route>
-            <Route path="/login" element={ <Login /> }></Route>
-            <Route path="/register" element={ <Register />} ></Route>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <br />
+            <br />
+            <br />
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/about" element={<About />}></Route>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/register" element={<Register />}></Route>
 
-            {/** NOT FOUND */}
-            <Route path="*" element={<NotFound />}></Route>
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+              {/** NOT FOUND */}
+              <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
